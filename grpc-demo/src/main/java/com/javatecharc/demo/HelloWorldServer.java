934f2771a2bd95416ce02e90ffe588bb81340909
Rajkumar ANGARE <rajangare@gmail.com>
2024-09-21 18:54:16 +0530

    GRPC Hello word program

---
+++ grpc-demo/src/main/java/com/javatecharc/demo/HelloWorldServer.java
@@ -0,0 +1,45 @@
+package com.javatecharc.demo;
+
+import com.javatecharc.demo.service.HelloWorldService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class HelloWorldServer {
+    private final Server server;
+
+    public HelloWorldServer(int port) {
+        server = ServerBuilder.forPort(port)
+                .addService(new HelloWorldService())
+                .build();
+    }
+
+    public void start() throws IOException {
+        server.start();
+        System.out.println("GRPC Server started on port " + server.getPort());
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("*** Shutting down gRPC server since JVM is shutting down");
+            HelloWorldServer.this.stop();
+            System.err.println("*** Server shut down");
+        }));
+    }
+
+    public void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    public void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HelloWorldServer server = new HelloWorldServer(8090);
+        server.start();
+        server.blockUntilShutdown();
+    }
+}
---
+++ grpc-demo/src/main/java/com/javatecharc/demo/client/HelloWorldClient.java
@@ -0,0 +1,22 @@
+package com.javatecharc.demo.client;
+
+import com.javatecharc.demo.proto.HelloRequest;
+import com.javatecharc.demo.proto.HelloResponse;
+import com.javatecharc.demo.proto.HelloWorldGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class HelloWorldClient {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8090)
+                .usePlaintext()
+                .build();
+
+        HelloWorldGrpc.HelloWorldBlockingStub stub = HelloWorldGrpc.newBlockingStub(channel);
+
+        HelloResponse response = stub.helloJavaTechArc(HelloRequest.newBuilder().setName("Java TechARC 3i...").build());
+        System.out.println("Response from server: " + response.getMessage());
+
+        channel.shutdown();
+    }
+}
