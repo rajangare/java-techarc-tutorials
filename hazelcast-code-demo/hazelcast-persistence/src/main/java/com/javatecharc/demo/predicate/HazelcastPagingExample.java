27becca29f5ee48a5780ad385f602f6a3b5d6246
Rajkumar ANGARE <rajangare@gmail.com>
2024-10-13 11:30:31 +0530

    Hazelcast Paging Predicates demo code - updated

--- hazelcast-code-demo/hazelcast-persistence/src/main/java/com/javatecharc/demo/predicate/HazelcastPagingExample.java
+++ hazelcast-code-demo/hazelcast-persistence/src/main/java/com/javatecharc/demo/predicate/HazelcastPagingExample.java
@@ -34,8 +34,8 @@ public class HazelcastPagingExample {
         // Default Ascending order, but it can be arranged as a comparator which helps to sort in descending order of id field
         Comparator<Map.Entry<Integer, User>> descendingComparator = new DescendingUserIdComparator();
 
-        //Create Paging predicate with page size 20
-        PagingPredicate<Integer, User> pagingPredicate = Predicates.pagingPredicate(equalPredicate, descendingComparator, 8);
+        //Create Paging predicate with page size 10
+        PagingPredicate<Integer, User> pagingPredicate = Predicates.pagingPredicate(equalPredicate, descendingComparator, 10);
 
         //Apply Paging predicate
         Collection<User> results = userMap.values(pagingPredicate);
@@ -60,7 +60,7 @@ public class HazelcastPagingExample {
         System.out.println("\nPage 3 -> ");
         results.forEach(System.out::println);
 
-        // a predicate which fetches 3 students for each page, natural order (see Student.compareTo()),
+        // a predicate which fetches 3 user for each page, natural order (see User.compareTo()),
         // does not filter out anything
         pagingPredicate = Predicates.pagingPredicate(3);
 
